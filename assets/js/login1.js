53a1699d92578d8d8dfccc5944257ee43307f7ee
1746061125@qq.com <wxiaoweia@163.com>
2020-09-11 14:22:58 +0800

    表单验证完成

--- assets/js/login1.js
+++ assets/js/login1.js
@@ -1,6 +1,7 @@
 $(function () {
     // 点击去注册 添加bnt框
     //点击注册
+    // var layui = layui.layui
     $('#btn-font').on('click', function () {
         // console.log(this);
         // show 显示
@@ -8,13 +9,9 @@ $(function () {
         console.log(1);
         $('#zc-box').show()
         $('#btn-zc').hide()
-        // $('#btn-dl').show()
-
     })
     //点击登录
     $('#dl-box').on('click', function () {
-        console.log('a');
-        // $('#btn-mm').hide()
         $('#zc-box').hide()
         $('#btn-zc').show()
 
@@ -22,6 +19,8 @@ $(function () {
 
     //自定义表单 验证
     //获取layui里的form属性去操作表单
+    // var httm = 'http://ajax.frontend.itheima.net'
+    //提示框
     var form = layui.form
     //给密码框编写自定义正则
     form.verify({
@@ -35,8 +34,52 @@ $(function () {
             var pwd = $('#zc-box [name=password]').val()
             console.log(pwd);
             if (pwd !== value) {
+                $('#zc-box [name=password]').val('');
                 return '俩次密码不一样'
+
             }
         }
+    })
+
+    //注册模块监听登录表单的提交事件
+    // $('#form_bd').submit(function (e) {
+    $('#form_bd').on('submit', function (e) {
+        // e.preventDefault();
+        e.preventDefault()
+        var data = {
+            username: $('#zc-box [name=identity]').val(),
+            password: $('#zc-box [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data,
+
+            function (res) {
+                console.log(res);
+                if (res.status !== 0) return console.log(res.message);
+            }
+
+        )
+        layer.msg('注册成功，请登录！')
+        $('#dl-box').click()
+    })
+    //监听登录页面提交信息
+    $('#btn-zc').on('submit', function (e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_login [name=identity]').val(),
+            password: $('#form_login [name=password]').val()
+        }
+        $.ajax({
+
+            url: 'http://ajax.frontend.itheima.net/api/login', data,
+            method: 'POST',
+            success: function (res) {
+                if (res.status !== 0) return layer.msg('登录失败!');
+                layer.msg('登录成功!')
+                console.log(res.token);
+                //将获取到得token值保存到localStorage里.token相当于一个钥匙
+                localStorage.setItem('token', res.token)
+                location.href = './index.html'
+            }
+        })
     })
 })
