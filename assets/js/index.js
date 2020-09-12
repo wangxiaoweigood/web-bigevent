7b20e85fed7e98bf59873e4265d7f0c5f17d7c3a
1746061125@qq.com <wxiaoweia@163.com>
2020-09-12 10:06:28 +0800

    优化防止用户直接输入路径登录模块

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,6 +32,16 @@ function getUser() {
             }
             console.log(res.data);
             renderAvatar(res.data)
+        },
+        //不论成功还是失败都会 调用complete回调函数
+        complete: function (res) {
+            // console.log(res, '执行了回调函数');
+            console.log(res.responseJSON);
+            //防止用户直接不登录，在网页上输入路径，只要后台访问不到数据库里得账号，直接清空tokrn值并且强制推到登录页面
+            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                localStorage.removeItem('token');
+                location.href = '/login.html'
+            }
         }
 
     })
